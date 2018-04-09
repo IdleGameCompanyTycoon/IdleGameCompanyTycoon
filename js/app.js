4ae988978a7a528ba8d1866d1dcee823fc95c87f
Dustin <dustin.gogoll@googlemail.com>
2018-04-09 16:29:46 +0200

    Added class based Assignments function

--- js/app.js
+++ js/app.js
@@ -3,47 +3,53 @@
 let money = 0;
 
 //Construcor Function for assignments
-function Assignments(max, pay, assignment){
-  this.assignment = assignment;
-  this.max = max;
-  this.earnings = pay;
-}
 
-//Adding the function to increase LoC count to the Constructors Prototype
-Assignments.prototype.increase = function increaseAssignment() {
-  let progressVal = 0;
-  let loc = 0;
-  let max = this.max;
-  let earning = this.earnings;
+class Assignments {
+  constructor(max, pay, assignment) {
+    this.assignment = assignment;
+    this.max = max;
+    this.earnings = pay;
+  }
 
-  return function() {
-    const assignment = document.querySelector('.assignment');
-    const progress = assignment.querySelector('.progress');
-    const text = assignment.querySelector('.assign-text');
-    console.log(max);
+  increase(increase = 1) {
+    let progressVal = 0;
+    let loc = 0;
+    let max = this.max;
+    let earning = this.earnings;
 
-    if(loc < max){
-      console.log('you');
-      progressVal += 100/max;
-      loc++;
-      progress.style.width = `${progressVal}%`;
-      text.textContent = `This is the first Task ${loc}/${max}`;
-    }
+    return () => {
+      const assignment = document.querySelector('.assignment');
+      const progress = assignment.querySelector('.progress');
+      const text = assignment.querySelector('.assign-text');
+      console.log(max);
 
-     if(loc == max){
-       loc++;
-      const moneyElem = document.querySelector('.money');
-      text.textContent = 'Finished';
-      money += earning;
-      moneyElem.textContent = `${money}$`;
-      taskOne= null;
+      if(loc < max){
+        console.log('you');
+        progressVal += 100/max*increase;
+        loc += increase;
+        progress.style.width = `${progressVal}%`;
+        text.textContent = `This is the first Task ${loc}/${max}`;
+      }
+
+       if(loc == max){
+         loc += increase;
+        const moneyElem = document.querySelector('.money');
+        text.textContent = 'Finished';
+        money += earning;
+        moneyElem.textContent = `${money}$`;
+        taskOne= null;
+      }
     }
   }
 }
 
+//Adding the function to increase LoC count to the Constructors Prototype
+
+
 //Test
 let taskOne = new Assignments(10, 10, 'The first Assignment');
+let taskTwo = new Assignments(10, 10, 'The Second Assignment');
 
 //LoC Button Event Handler
 const locButton = document.querySelector('.loc');
-locButton.addEventListener('click', taskOne.increase());
+locButton.addEventListener('click', taskOne.increase(2));
