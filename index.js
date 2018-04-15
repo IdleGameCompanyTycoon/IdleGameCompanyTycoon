1e76a63e42b11d8d6767f8bf5bce4e6f81f1ba85
Dustin <dustin.gogoll@googlemail.com>
2018-04-15 15:09:35 +0200

    test fix on index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var http = require('http');
+const PORT = process.env.PORT;
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
 res.end('Hi everybody!');
 });
-server.listen(8080);
+server.listen(PORT);
