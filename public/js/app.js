d43aa2648c511b67b11d76bbb527982033800041
Dustin <dustin.gogoll@googlemail.com>
2018-04-23 15:03:03 +0200

    New MVC pattern partwise finished and added auto contract generation via database

--- public/js/app.js
+++ public/js/app.js
@@ -1,90 +1,156 @@
-
-
-//Global Variables
-let money = 0;
-let availableTasks  =  [];
-
-//Construcor Function for assignments
-
-class Assignments {
-  constructor(codeSize, pay, assignment) {
+//Class to create new contracts
+class Contracts {
+  constructor(codeSize, pay, assignment, descr) {
     this.assignment = assignment;
     this.codeSize = codeSize;
     this.earnings = pay;
+    this.description = descr;
+    this.progressVal = 0;
+    this.domElem = gameApp.newContractDomElem(this);
   }
 
   increase(increase = 1) {
-    let progressVal = 0;
+
     let loc = 0;
     let codeSize = this.codeSize;
     let earning = this.earnings;
 
     return () => {
-      const assignment = document.querySelector('.assignment');
-      const progress = assignment.querySelector('.progress');
-      const text = assignment.querySelector('.assign-text');
-      console.log(codeSize);
 
       if(loc < codeSize){
-        console.log('you');
-        progressVal += 100/codeSize*increase;
+        this.progressVal += 100/codeSize*increase;
         loc += increase;
-        progress.style.width = `${progressVal}%`;
-        text.textContent = `This is the first Task ${loc}/${codeSize}`;
       }
 
        if(loc == codeSize){
          loc += increase;
-        const moneyElem = document.querySelector('.money');
-        text.textContent = 'Finished';
-        money += earning;
-        moneyElem.textContent = `${money}$`;
-        taskOne = null;
+        model.money += earning;
       }
     }
   }
 }
 
-//Function to add the Available Tasks to the DOM
 
-function addTaskToDom(object) {
-  const tasksElement = document.querySelector('.secondary');
-  const container = document.createElement('div');
-  container.className = 'available-tasks';
 
-  container.innerHTML = `${object.assignment} <br><br>
-                        Scale: ${object.codeSize} LoC <br>
-                        Reward: ${object.earnings}$`;
-  tasksElement.appendChild(container);
+//Fetch requests
+const dataRequests = {
+  getContract: function () {
+        fetch('/getData', {
+          method: 'GET',
+          })
+          .then(response => response.json())
+          .then(response => {
+            return response.rows[0];
+          })
+        }
+
 }
 
-//Test
-let taskOne = new Assignments(10, 10, 'The first Assignment');
-let taskTwo = new Assignments(10, 10, 'The Second Assignment');
-let taskThree = new Assignments(10, 10, 'The Third Assignment');
 
-availableTasks.push(taskOne);
-availableTasks.push(taskTwo);
-availableTasks.push(taskThree);
 
-(() => {
-  for(let task of availableTasks){
-    addTaskToDom(task);
+// Model for Data
+const model = {
+    money: 0,
+    availableContracts: [],
+    activeContracts: [],
+    createContract: function(obj){
+      return new Contracts(obj.codesize, obj.payment, obj.contract, obj.description);
+    }
+}
+
+
+
+//Controller for communication between model and view
+class GameController {
+  constructor(gameView) {
+    this.gameView = gameView;
   }
-})()
 
-//LoC Button Event Handler
-const locButton = document.querySelector('.loc');
-locButton.addEventListener('click', taskOne.increase(2));
+  //Create a new Contract Dom element
+  newContractDomElem(obj) {
+    return this.gameView.createNewContractDom(obj);
+  }
 
-//Test
+  //Get all available tasks from the model
+  getAvailableContracts() {
+    return model.availableContracts;
+  }
 
-function get() {
-  fetch('/getData', {
-    method: 'GET',
-  })
-    .then(response => response.json())
-    .then(response => {
-      availableTasks = response.rows;
-    })
+  //Get all active tasks from the model
+  getActiveContracts() {
+    return model.activeContracts;
+  }
+
+  //Get the money
+  getMoney() {
+    return model.money;
+  }
+
+  //Create contract interval
+  createContracts() {
+    setInterval(function() {
+      if(model.availableContracts.length < 6) {
+        dataRequests.getContract()
+                    .then(model.createContract(result))
+                    .then(function(result) {
+                      model.availableContracts.push(result);
+                      this.gameView.addAvailableContracts(result);
+                    })
+      }
+    }, 10000)
+  }
+
+  init() {
+    this.gameView.init();
+  }
 }
+
+
+
+//View rendering
+class GameView {
+  init() {
+    const allAvailableContracts = gameApp.getAvailableContracts();
+    allAvailableContracts.forEach(obj => this.addAvailableContracts(obj));
+  }
+
+  //Creates new Dom element for contracts
+  createNewContractDom(object) {
+    const container = document.createElement('div');
+    container.className = 'available-tasks';
+
+    container.innerHTML = `${object.assignment} <br><br>
+                          Scale: ${object.codeSize} LoC <br>
+                          Reward: ${object.earnings}$`;
+    return container;
+  }
+
+  //Creates active dom element for Contracts
+  createActiveContractDom(object) {
+    const newActiveContract = document.createElement('div');
+    const newProgressBar = document.createElement('div');
+    const newContractText = document.createElement('span');
+
+    newActiveContract.classList.add('assignment');
+    newProgressBar.classList.add('progress');
+    newContractText.classList.add('assign-text');
+
+    newContractText.textContent = `${object.assignment} 0/${object.codeSize}`;
+
+    newActiveContract.append(newProgressBar, newContractText);
+
+    return newActiveContract;
+  }
+
+  //Add available Task to domElem
+  addAvailableContracts(obj) {
+    document.querySelector('.secondary').appendChild(obj.domElem);
+  }
+}
+
+
+//Init everything
+const gameView = new GameView();
+const gameApp = new GameController(gameView);
+
+gameApp.init();
