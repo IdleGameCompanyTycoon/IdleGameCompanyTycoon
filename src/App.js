b01905b06c971a38311b33e9fbcf9418c0dd9a20
Nalun <raphaelgrimm@icloud.com>
2018-09-05 21:23:23 +0200

    Removed the logo.svg import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Offline from './components/containers/Offline';
 import Online from './components/containers/Online';
