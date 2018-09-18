bf5b6b620f1fc82c6dbaa84e4d3818e5f21345f4
Dustin Gogoll <dustin.gogoll@googlemail.com>
2018-09-18 20:38:25 +0200

    Storing the mode selection

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ class App extends Component {
     offline: false
   }
 
+  componentDidMount() {
+    let savedSelection = localStorage.getItem('selection');
+    console.log(savedSelection);
+    if(savedSelection) {
+      savedSelection = JSON.parse(savedSelection);
+      this.setState({
+        ...savedSelection
+      })
+    }
+  }
+
   // TODO: Save the current selection to the local storage
   setToOnline = () => {
     this.setState({
       selected: true,
       online: true,
       offline: false
-    })
+    }, this.saveToLocalStorage)
   }
 
   setToOffline = () => {
@@ -23,7 +34,13 @@ class App extends Component {
       selected: true,
       online: false,
       offline: true
-    })
+    }, this.saveToLocalStorage)
+  }
+
+  saveToLocalStorage = () => {
+    console.log('test')
+    let selection = JSON.stringify(this.state);
+    localStorage.setItem('selection', selection);
   }
 
   render() {
