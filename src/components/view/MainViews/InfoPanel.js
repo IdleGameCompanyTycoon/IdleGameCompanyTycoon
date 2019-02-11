fcca45ec77fabc723e6416c884fe60e548db1bfe
Rafael <rafael1998@gmx.de>
2019-02-11 19:40:50 +0100

    merge conflicts

--- src/components/view/MainViews/InfoPanel.js
+++ src/components/view/MainViews/InfoPanel.js
@@ -4,22 +4,12 @@ import '../../../assets/css/App-main-view-infopanel.css';
 const InfoPanel = (props) => {
   return (
     <div className='App-info-panel'>
-<<<<<<< HEAD
-        <div className="App-info-panel-data">
-          <p>Money: {props.money}</p>
-          <p>Day {props.date.day}, Month {props.date.month}, Year {props.date.year}</p>
-        </div>
-
-        <button className="App-info-panel-home-button"
-                onClick={() => {props.goToHome()}}>Home</button>
-=======
           <p className="App-info-panel-money">Money: {props.money}</p>
           <p className="App-info-panel-locgen">LoC per Month: XXXXX{/*TODO: TO BE ADDED*/}</p>
           <p className="App-info-panel-expenses">Expenses: XXXXX{/*TODO: TO BE ADDED*/}</p>
           <p className="App-info-panel-date">Date: {props.date.month}.{props.date.day}.{props.date.year}</p>
         <button className="App-info-panel-homebutton"
                 onClick={props.goToHome}>Home</button>
->>>>>>> b5d6064b5c234f2a968d2d859ed676baa4d2a805
     </div>
   )
 }
--- src/components/view/SubViews/Employee.js
+++ src/components/view/SubViews/Employee.js
@@ -1,28 +1,6 @@
 import React from 'react';
 
 const Employee = (props) => {
-<<<<<<< HEAD
-  let working;
-  if(props.employee.working){
-    working = "Working";
-  }else{
-    working =  "Fired";
-  }
-    return (
-
-      <div className="App-">
-        {/* TODO: Add Missing data*/}
-        <p>{working} </p>
-        <p>{`${props.employee.givenName} ${props.employee.lastName}`}</p>
-        <p>Work speed: {props.employee.loc} LoC</p>
-        <p>Salary: {props.employee.payment}€</p>
-        <img src={props.employee.imgUrl} alt="picture of employee"/>
-          <button
-            onClick={() => props.action("fireEmployee", props.employee)
-        }>Fire</button>
-      </div>
-    )
-=======
   return (
     <div>
       <hr>
@@ -52,7 +30,6 @@ const Employee = (props) => {
         </hr>
     </div>
   )
->>>>>>> b5d6064b5c234f2a968d2d859ed676baa4d2a805
 }
 
 export default Employee;
