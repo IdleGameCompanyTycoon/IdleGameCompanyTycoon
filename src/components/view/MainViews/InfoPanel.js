f9bbb0280e8f138f3ec86f84397b7f626cfabd92
Dustin Gogoll <dustin.gogoll@googlemail.com>
2019-11-04 10:21:37 +0100

    Added Infor panel LoC per Day and a proper currency format for euros

--- src/components/view/MainViews/InfoPanel.js
+++ src/components/view/MainViews/InfoPanel.js
@@ -4,9 +4,9 @@ import '../../../assets/css/App-main-view-infopanel.css';
 const InfoPanel = (props) => {
   return (
     <div className='App-info-panel'>
-          <p className="App-info-panel-money">Money: {props.money}</p>
-          <p className="App-info-panel-locgen">LoC per Month: {props.locPerMonth}</p>
-          <p className="App-info-panel-expenses">Expenses: {props.expenses}</p>
+          <p className="App-info-panel-money">Money: {Number(props.money).toLocaleString("es-ES", {minimumFractionDigits: 2})}€</p>
+          <p className="App-info-panel-locgen">LoC per Day/Month: {(props.locPerMonth/30).toFixed(2)}/{props.locPerMonth}</p>
+          <p className="App-info-panel-expenses">Expenses: {Number(props.expenses).toLocaleString("es-ES", {minimumFractionDigits: 2})}€</p>
           <p className="App-info-panel-date">Date: {props.date.month}.{props.date.day}.{props.date.year}</p>
         <button className="App-info-panel-homebutton"
                 onClick={props.goToHome}>Home</button>
