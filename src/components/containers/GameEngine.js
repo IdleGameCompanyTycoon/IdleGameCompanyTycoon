8c2830722ba5ab252f3af786dc4a2d496a5922aa
Dustin Gogoll <33902083+DustinGDev@users.noreply.github.com>
2019-12-15 12:51:57 +0100

    Update GameEngine.js

--- src/components/containers/GameEngine.js
+++ src/components/containers/GameEngine.js
@@ -3,7 +3,7 @@ import '../../assets/css/App-main-view.css';
 import { updateDate, onMonthChange } from './engineAPIs/mainAPI.js';
 import { initApplicationGen, initContractsGen } from './engineAPIs/dataFetchApi.js';
 import Main from '../view/Main.js';
-import Noti from '../view/MainViews/Notification.js';
+import Noti from '../view/MainViews/Notification.js'; // FIXME: USE FULL FUNCTION NAMES GOD DAMN IT! ^^
 import InfoPanel from '../view/MainViews/InfoPanel.js';
 import AnimationFrame from '../view/MainViews/AnimationFrame.js';
 import Navigation from '../view/MainViews/Navigation.js';
