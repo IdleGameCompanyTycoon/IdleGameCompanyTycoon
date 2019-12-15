5ae405871cd24a9795b70fc1877226ec47460a05
Dustin Gogoll <33902083+DustinGDev@users.noreply.github.com>
2019-12-15 12:52:36 +0100

    Update Notification.js

--- src/components/view/MainViews/Notification.js
+++ src/components/view/MainViews/Notification.js
@@ -3,7 +3,7 @@ import '../../../assets/css/App-notification.css';
 
 const Noti = (props) => {
   if(!props.notif){return null}
-  if(props.notif === "info"){
+  if(props.notif === "info"){ // FIXME: Notif? Full names!
     return(
       <div className="App-noti">
         <div className="App-close"
